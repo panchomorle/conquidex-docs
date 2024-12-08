80f74c2a22c7017c38aa33974a38487131c63091
panchomorle <eljuampimorales@gmail.com>
2024-12-08 12:23:54 -0300

    design update - honors

--- II. DESIGN MODEL/DB queries.sql
+++ II. DESIGN MODEL/DB queries.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "persons" (
-  "id" integer,
+  "id" integer PRIMARY KEY,
   "name" varchar,
   "surname" varchar,
   "nickname" varchar,
@@ -7,42 +7,48 @@ CREATE TABLE "persons" (
 );
 
 CREATE TABLE "clubs" (
-  "id" integer,
+  "id" integer PRIMARY KEY,
   "name" varchar,
+  "street" varchar,
+  "street_number" int,
+  "city" varchar,
+  "state" varchar,
+  "country" varchar,
   "desc" varchar,
   "phone_number" varchar
 );
 
 CREATE TABLE "socials" (
-  "id" integer,
+  "id" integer PRIMARY KEY,
   "name" varchar,
   "svg_url" varchar
 );
 
 CREATE TABLE "club_socials" (
-  "id" integer,
-  "url" string,
+  "id" integer PRIMARY KEY,
+  "url" varchar,
   "social_id" integer,
   "club_id" integer
 );
 
 CREATE TABLE "roles" (
-  "id" integer,
-  "name" string
+  "id" integer PRIMARY KEY,
+  "name" varchar
 );
 
 CREATE TABLE "roles_persons" (
   "id_person" integer,
-  "id_role" integer
+  "id_role" integer,
+  PRIMARY KEY ("id_person", "id_role")
 );
 
 CREATE TABLE "units" (
-  "id" integer,
+  "id" integer PRIMARY KEY,
   "name" varchar
 );
 
 CREATE TABLE "classes" (
-  "id" integer,
+  "id" integer PRIMARY KEY,
   "name" varchar
 );
 
@@ -50,35 +56,63 @@ CREATE TABLE "class_items" (
   "class_id" integer,
   "id" integer,
   "name" varchar,
-  "desc" varchar
+  "desc" varchar,
+  PRIMARY KEY ("class_id", "id")
 );
 
 CREATE TABLE "person_class_items" (
   "person_id" integer,
+  "class_id" integer,
   "item_id" integer,
-  "status" varchar
+  "status" boolean,
+  PRIMARY KEY ("person_id", "class_id", "item_id")
 );
 
 CREATE TABLE "pathfinder_units" (
   "person_id" integer,
-  "unit_id" integer
+  "unit_id" integer,
+  PRIMARY KEY ("person_id", "unit_id")
 );
 
 CREATE TABLE "counselor_units" (
   "person_id" integer,
   "unit_id" integer,
-  "assignment_date" date
+  "assignment_date" date,
+  PRIMARY KEY ("person_id", "unit_id")
 );
 
 CREATE TABLE "staff_units" (
   "person_id" integer,
   "unit_id" integer,
-  "assignment_date" date
+  "assignment_date" date,
+  PRIMARY KEY ("person_id", "unit_id")
 );
 
 CREATE TABLE "person_classes" (
   "person_id" integer,
-  "class_id" integer
+  "class_id" integer,
+  PRIMARY KEY ("person_id", "class_id")
+);
+
+CREATE TABLE "honor" (
+  "honor_category_id" varchar,
+  "id" integer,
+  "name" varchar,
+  "level" integer,
+  PRIMARY KEY ("honor_category_id", "id")
+);
+
+CREATE TABLE "honor_category" (
+  "id" varchar PRIMARY KEY,
+  "name" varchar
+);
+
+CREATE TABLE "person_honor" (
+  "person_id" integer,
+  "honor_category_id" varchar,
+  "honor_id" integer,
+  "status" boolean,
+  PRIMARY KEY ("person_id", "honor_category_id", "honor_id")
 );
 
 ALTER TABLE "club_socials" ADD FOREIGN KEY ("club_id") REFERENCES "clubs" ("id");
@@ -112,3 +146,13 @@ ALTER TABLE "pathfinder_units" ADD FOREIGN KEY ("person_id") REFERENCES "persons
 ALTER TABLE "counselor_units" ADD FOREIGN KEY ("person_id") REFERENCES "persons" ("id");
 
 ALTER TABLE "staff_units" ADD FOREIGN KEY ("person_id") REFERENCES "persons" ("id");
+
+ALTER TABLE "person_honor" ADD FOREIGN KEY ("honor_id") REFERENCES "honor" ("id");
+
+ALTER TABLE "person_honor" ADD FOREIGN KEY ("honor_category_id") REFERENCES "honor" ("honor_category_id");
+
+ALTER TABLE "person_honor" ADD FOREIGN KEY ("person_id") REFERENCES "persons" ("id");
+
+ALTER TABLE "honor" ADD FOREIGN KEY ("id") REFERENCES "honor_category" ("id");
+
+ALTER TABLE "person_class_items" ADD FOREIGN KEY ("class_id") REFERENCES "class_items" ("class_id");
